1cb86931ce0d69225075ae4e1bf1245adce3d5d4
Nathan Hughes <20962973+Killerherts@users.noreply.github.com>
2023-05-24 08:33:20 -0500

    Update and rename feedTimes.js to feedLightTimes.js
    add light control to feed times function

--- feedLightTimes.js
+++ feedLightTimes.js
@@ -1,21 +1,16 @@
-// Import Home Assistant state data
 const d = global.get('homeassistant').homeAssistant;
+let flipStarted = d.states["input_boolean.flip_to_flower_side1"].state === "on"; // Convert the state to a boolean
+let numberOfFeeds = d.states["input_number.number_of_feeds_side1"].state;
+let startFeedTime = d.states["input_datetime.lights_on_time_side1"].state;
+let generativeSteering = d.states["input_boolean.generative_steering_side1"].state === "on";
+let currentTime = "";
+let lightOffTime = "";
+let lightOnTime = "";
 
-// Retrieve and process necessary state data
-let flipStarted = d.states["input_boolean.has_flip_to_flower_started"].state === "on"; // Convert the state to a boolean
-let numberOfFeeds = d.states["input_number.number_of_feeds_flower_area"].state;
-let startFeedTime = d.states["input_datetime.lights_on_time"].state;
-let generativeSteering = d.states["input_boolean.generative_steering_flower"].state === "on";
-// Log relevant state data for debugging
-console.log(startFeedTime, numberOfFeeds, flipStarted, generativeSteering);
+//console.log(startFeedTime, numberOfFeeds, flipStarted, generativeSteering);
 
-// Calculate feed times using the state data
 let feedTimes = calculateFeedTime(flipStarted, numberOfFeeds, startFeedTime, generativeSteering);
-/**
- * Convert a time string to the number of seconds since midnight.
- * @param {string} timeString - The time string to convert.
- * @return {number} The time in seconds.
- */
+
 function convertTime(timeString) {
     let time = new Date(timeString);
     let isUtc = timeString.includes("Z") || timeString.includes("+") || timeString.includes("-");
@@ -35,25 +30,19 @@ function convertTime(timeString) {
         minutes = time.getMinutes();
         seconds = time.getSeconds();
     }
-
+    currentTime = hours * 60 * 60 + minutes * 60 + seconds;
     return hours * 60 * 60 + minutes * 60 + seconds;
 }
-/**
- * Calculate the feed times based on the given input parameters.
- * @param {boolean} flipStarted - Whether the flip to flower has started.
- * @param {number} numberOfFeeds - The number of feedings in the flower area.
- * @param {string} startFeedTime - The start time of the feedings.
- * @param {boolean} generativeSteering - Whether generative steering is enabled.
- * @return {string[]} An array of feed times as strings in the format "HH:mm:ss".
- */
 function calculateFeedTime(flipStarted, numberOfFeeds, startFeedTime, generativeSteering) {
     let lastFeedTime = 0;
     let startFeedTimeSeconds = convertTime(startFeedTime);
-
+    lightOnTime = convertTime(startFeedTime);
     if (flipStarted) {
         lastFeedTime = startFeedTimeSeconds + (12 * 60 * 60);
+        lightOffTime = lastFeedTime
     } else {
         lastFeedTime = startFeedTimeSeconds + (18 * 60 * 60);
+        lightOffTime = lastFeedTime
     }
 
     if (generativeSteering) { // Add this condition
@@ -61,6 +50,7 @@ function calculateFeedTime(flipStarted, numberOfFeeds, startFeedTime, generative
         lastFeedTime -= 3 * 60 * 60;
     } else { // Add this else statement
         startFeedTimeSeconds += 1 * 60 * 60;
+        lastFeedTime -= 1 * 60 * 60;
     }
 
     let feedTimes = [];
@@ -84,20 +74,14 @@ function calculateFeedTime(flipStarted, numberOfFeeds, startFeedTime, generative
     return feedTimes;
 }
 
-/**
- * Check if it's time to feed based on the current time and feed times.
- * @param {string[]} feedTimes - An array of feed times as strings in the format "HH:mm:ss".
- * @return {string} "feed" if it's time to feed, otherwise "no feed".
- */
-
 function checkFeedTime(feedTimes) {
-    let currentTime = new Date();
+    currentTime = new Date();
     let currentHour = currentTime.getHours();
     let currentMinute = currentTime.getMinutes();
     let currentSecond = currentTime.getSeconds();
     let currentTotalSeconds = (currentHour * 60 * 60) + (currentMinute * 60) + currentSecond;
 
-    console.log("Current time (seconds):", currentTotalSeconds);
+    // console.log("Current time (seconds):", currentTotalSeconds);
 
     let numberOfFeeds = feedTimes.length;
     let lastFeedTime = context.get("lastFeedTime") || 0;
@@ -120,16 +104,27 @@ function formatFeedTimes(feedTimes) {
     let table = formattedFeedTimes.join("\n");
     return table;
 }
-// Format feed times for display and check if it's time to feed
 
 let formattedFeedTimes = formatFeedTimes(feedTimes);
 let feedAction = checkFeedTime(feedTimes);
 
-// Set the node status and output based on whether it's time to feed
+if (currentTime >= lightOnTime && d.states["switch.veg_light_switch"]?.state === "off") {
+    node.status({ fill: "green", shape: "dot", text: "Turn On" });
+    msg.payload.lightAction = 'Turned On';
+    console.log('Lights turned on.');
+    return [null, null, { payload: "turn_on" }];
+}
+
+if (currentTime >= lightOffTime && d.states["switch.veg_light_switch"]?.state === "on") {
+    node.status({ fill: "red", shape: "dot", text: "Turn Off" });
+    msg.payload.lightAction = 'Turned Off';
+    console.log('Lights turned off.');
+    return [null, null, { payload: "turn_off" }];
+}
 if (feedAction === "feed") {
-    node.status({ fill: "green", shape: "dot", text: "Feed" });
-    return [{ payload: "feed" }, null];
+    node.status({ fill: "yellow", shape: "dot", text: "Feed" });
+    return [{ payload: "feed" }, null, null];
 } else {
     node.status({ fill: "blue", shape: "dot", text: "No Feed" });
-    return [null, { payload: formattedFeedTimes }];
+    return [null, { payload: formattedFeedTimes }, null];
 }
