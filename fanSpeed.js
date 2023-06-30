7e1a5eecb3bdb7321a3a9cde83f3d95d4b627396
Nathan Hughes <20962973+Killerherts@users.noreply.github.com>
2023-06-30 09:53:22 -0500

    Update fanSpeed.js

--- fanSpeed.js
+++ fanSpeed.js
@@ -2,33 +2,29 @@ const currentRh = msg.rh;
 const requiredRh = msg.req_rh;
 const currentTemp = msg.temp;
 const desiredTemp = msg.desiredTemperature;
-const currentBrightness = msg.brightness || 0;
-const maxBrightness = 105;
-const minBrightness = 0;
-const brightnessScaling = 5; // Set the brightness scaling to a static value of 5
+const currentBrightness = msg.brightness;
 
-// Calculate the temperature difference
-const tempDifference = currentTemp - desiredTemp;
+//node.warn("current brightness: " + currentBrightness + ", current rh: " + currentRh + ', current temp: ' + currentTemp);
 
-// Calculate the proportion of the temperature difference relative to the temperature range
-// Proportion will be 0 when the temperature is equal to or below the desired temperature
-const proportion = Math.max(tempDifference / Math.max(Math.abs(tempDifference), 1), 0);
+const maxBrightness = 255;
+const minBrightness = 1;
+const brightnessScaling = 8;
 
-// Calculate the new brightness based on the proportion and the static scaling value
-const brightnessChange = proportion * (maxBrightness - minBrightness) * brightnessScaling;
+let brightnessAdjustment = brightnessScaling;
 
-// Calculate brightness based on Rh
-let rhBrightness = Math.round(currentRh / requiredRh * maxBrightness);
-
-if (currentTemp < desiredTemp || currentRh < requiredRh) {
-    // Set brightness to 0 if the current temperature is below the desired temperature or if the current RH is below the desired RH
-    msg.brightness = 0;
-} else if (tempDifference > 0 && rhBrightness > msg.brightness) {
-    // Increase brightness if the temperature is above the desired value and Rh brightness is higher
-    msg.brightness = Math.round(Math.min(rhBrightness + brightnessChange, maxBrightness));
-} else if (rhBrightness > msg.brightness) {
-    // Update brightness if Rh brightness is higher
-    msg.brightness = rhBrightness;
+if (currentTemp > desiredTemp || currentRh > requiredRh) {
+    // Either temperature or humidity is above desired level
+    msg.brightness = Math.min(currentBrightness + brightnessAdjustment, maxBrightness);
+    node.warn("Either temperature or humidity is above desired level. Increasing brightness by brightness scaling. New Level is " + Math.round((msg.brightness / maxBrightness) * 100));
+    msg.payload = msg.brightness;
+    return msg;
+} else if (currentTemp < desiredTemp && currentRh < requiredRh && currentBrightness != (Math.max(currentBrightness - (brightnessAdjustment * 2), minBrightness))) {
+    // Both temperature and humidity are below desired levels
+    msg.brightness = Math.max(currentBrightness - (brightnessAdjustment * 2), minBrightness);
+    node.warn("Both temperature and humidity are below desired levels. Decreasing brightness by brightness scaling. New Brightness is " + Math.round((msg.brightness / maxBrightness) * 100));
+    msg.payload = msg.brightness;
+    return msg;
+} else {
+    // No action required
+    node.warn("No action required. Keeping the brightness unchanged.");
 }
-
-return msg;
