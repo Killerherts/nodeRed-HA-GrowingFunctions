02123cc3b27a9aa8ab3b95fb25a4e3db116f76fd
Nathan Hughes <20962973+Killerherts@users.noreply.github.com>
2023-07-01 10:06:43 -0500

    Update fanSpeed.js
    Changed to make script ramp fan up or down as a scale based every run.
    Also made it so the ramp down was 2x are aggressive as the ramp down so we keep numbers high

--- fanSpeed.js
+++ fanSpeed.js
@@ -4,7 +4,7 @@ const currentTemp = msg.temp;
 const desiredTemp = msg.desiredTemperature;
 const currentBrightness = msg.brightness;
 
-//node.warn("current brightness: " + currentBrightness + ", current rh: " + currentRh + ', current temp: ' + currentTemp);
+// node.warn("current brightness: " + currentBrightness + ", current rh: " + currentRh + ', current temp: ' + currentTemp);
 
 const maxBrightness = 255;
 const minBrightness = 1;
@@ -15,15 +15,19 @@ let brightnessAdjustment = brightnessScaling;
 if (currentTemp > desiredTemp || currentRh > requiredRh) {
     // Either temperature or humidity is above desired level
     msg.brightness = Math.min(currentBrightness + brightnessAdjustment, maxBrightness);
-    node.warn("Either temperature or humidity is above desired level. Increasing brightness by brightness scaling. New Level is " + Math.round((msg.brightness / maxBrightness) * 100));
-    msg.payload = msg.brightness;
-    return msg;
-} else if (currentTemp < desiredTemp && currentRh < requiredRh && currentBrightness != (Math.max(currentBrightness - (brightnessAdjustment * 2), minBrightness))) {
+    if (msg.brightness !== currentBrightness) {
+        node.warn("Either temperature or humidity is above desired level. Increasing brightness by brightness scaling. New Level is " + Math.round((msg.brightness / maxBrightness) * 100));
+        msg.payload = msg.brightness;
+        return msg;
+    }
+} else if (currentTemp < desiredTemp && currentRh < requiredRh && currentBrightness !== Math.max(currentBrightness - (brightnessAdjustment * 2), minBrightness)) {
     // Both temperature and humidity are below desired levels
     msg.brightness = Math.max(currentBrightness - (brightnessAdjustment * 2), minBrightness);
-    node.warn("Both temperature and humidity are below desired levels. Decreasing brightness by brightness scaling. New Brightness is " + Math.round((msg.brightness / maxBrightness) * 100));
-    msg.payload = msg.brightness;
-    return msg;
+    if (msg.brightness !== currentBrightness) {
+        node.warn("Both temperature and humidity are below desired levels. Decreasing brightness by brightness scaling. New Brightness is " + Math.round((msg.brightness / maxBrightness) * 100));
+        msg.payload = msg.brightness;
+        return msg;
+    }
 } else {
     // No action required
     node.warn("No action required. Keeping the brightness unchanged.");
