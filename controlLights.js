c721b58ff3e2fc3cf3ddc56c6fca6d4900b97205
Nathan Hughes <20962973+Killerherts@users.noreply.github.com>
2023-10-25 12:38:16 -0500

    Update controlLights.js

--- controlLights.js
+++ controlLights.js
@@ -1,9 +1,18 @@
-// Get the Home Assistant object
-const d = global.get('homeassistant').homeAssistant;
-let flipStarted = d.states["input_boolean.side2_flip_to_flower"].state;
-let switchState = d.states["switch.600_rspec"].state;
-let lightsOnTime = d.states["input_datetime.side_2_lights_on_time"].state;
-let debugMode = true;
+const HA_CONFIG = {
+    homeAssistantObject: 'homeassistant.homeAssistant',
+    flipStartedEntity: 'input_boolean.side1_filp_to_flower',
+    switchStateEntity: 'switch.side_1_grow_light',
+    lightsOnTimeEntity: 'input_datetime.side_1_lights_on_time',
+    lightControlEntity: 'switch.side_1_grow_light',
+    serviceDomain: 'switch'
+};
+
+// Fetch the Home Assistant object and states
+const d = global.get(HA_CONFIG.homeAssistantObject);
+let flipStarted = d.states[HA_CONFIG.flipStartedEntity].state;
+let switchState = d.states[HA_CONFIG.switchStateEntity].state;
+let lightsOnTime = d.states[HA_CONFIG.lightsOnTimeEntity].state;
+let debugMode = false;
 
 // Function to log debug messages with labels
 function logDebug(label, value) {
@@ -39,22 +48,38 @@ function parseTimeToSeconds(timeStr) {
 
 // Function to calculate the lights control logic
 function calculateLightsControl() {
-    var currentTime = new Date().getTime() / 1000; // Get current time in seconds
+    // Get the current date and time
+    const currentDate = new Date();
+
+    // Extract hours, minutes, and seconds
+    const currentHours = currentDate.getHours();
+    const currentMinutes = currentDate.getMinutes();
+    const currentSeconds = currentDate.getSeconds();
+
+// Calculate the total seconds
+    const currentTime = (currentHours * 3600) + (currentMinutes * 60) + currentSeconds;
+
+
     // Parse time to seconds
     let lightsOnTimeInSeconds = parseTimeToSeconds(lightsOnTime);
 
     // Calculate lightsOffTimeInSeconds based on flipStarted
     let lightsOffTimeInSeconds;
     if (flipStarted) {
-        lightsOffTimeInSeconds = lightsOnTimeInSeconds + 12 * 3600;
+        lightsOffTimeInSeconds = lightsOnTimeInSeconds + (12 * 3600);
     } else {
-        lightsOffTimeInSeconds = lightsOnTimeInSeconds + 18 * 3600;
+        lightsOffTimeInSeconds = lightsOnTimeInSeconds + (18 * 3600);
     }
+
+    // Ensure lightsOffTimeInSeconds doesn't exceed 24 hours
+    lightsOffTimeInSeconds = lightsOffTimeInSeconds % (24 * 3600);
 
     // Debugging: Log the values of lightsOnTimeInSeconds, lightsOffTimeInSeconds, and flipStarted
     logDebug('lightsOnTimeInSeconds:', lightsOnTimeInSeconds);
     logDebug('lightsOffTimeInSeconds:', lightsOffTimeInSeconds);
     logDebug('flipStarted:', flipStarted);
+    logDebug('current time ', currentTime);
+    logDebug('Switch State: ', switchState);
 
     if (lightsOnTimeInSeconds < lightsOffTimeInSeconds) {
         // Lights come on and go off on the same day
@@ -62,9 +87,9 @@ function calculateLightsControl() {
             // Within the time range and switch is off, turn on the lights
             if (switchState == 'off') {
                 msg.payload = {
-                    service_domain: 'homeassistant',
+                    service_domain: HA_CONFIG.serviceDomain,
                     service: 'turn_on',
-                    entity_id: 'switch.600_rspec'
+                    entity_id: HA_CONFIG.lightControlEntity
                 };
                 // Debugging: Log that the lights are being turned on
                 logDebug('Turning on the lights', '');
@@ -75,9 +100,9 @@ function calculateLightsControl() {
             // Outside the time range or switch is already on, turn off the lights
             if (switchState == 'on') {
                 msg.payload = {
-                    service_domain: 'homeassistant',
+                    service_domain: HA_CONFIG.serviceDomain,
                     service: 'turn_off',
-                    entity_id: 'switch.600_rspec'
+                    entity_id: HA_CONFIG.lightControlEntity
                 };
                 // Debugging: Log that the lights are being turned off
                 logDebug('Turning off the lights', '');
@@ -90,10 +115,11 @@ function calculateLightsControl() {
         if (currentTime >= lightsOnTimeInSeconds || currentTime < lightsOffTimeInSeconds) {
             // Within the time range and switch is off, turn on the lights
             if (switchState == 'off') {
+                
                 msg.payload = {
-                    service_domain: 'homeassistant',
+                    service_domain: HA_CONFIG.serviceDomain,
                     service: 'turn_on',
-                    entity_id: 'switch.600_rspec'
+                    entity_id: HA_CONFIG.lightControlEntity
                 };
                 // Debugging: Log that the lights are being turned on
                 logDebug('Turning on the lights', '');
@@ -104,9 +130,9 @@ function calculateLightsControl() {
             // Outside the time range or switch is already on, turn off the lights
             if (switchState == 'on') {
                 msg.payload = {
-                    service_domain: 'homeassistant',
+                    service_domain: HA_CONFIG.serviceDomain,
                     service: 'turn_off',
-                    entity_id: 'switch.600_rspec'
+                    entity_id: HA_CONFIG.lightControlEntity
                 };
                 // Debugging: Log that the lights are being turned off
                 logDebug('Turning off the lights', '');
@@ -115,11 +141,12 @@ function calculateLightsControl() {
             }
         }
     }
+}
 
     // No action needed, switch is already in the desired position
     // Debugging: Log that no action is needed
-    logDebug('No action needed');
-}
+logDebug('No action needed');
+
 
 // Call the calculateLightsControl function to perform the lights control logic
 calculateLightsControl();
