20d990aaa183005e94e0397640bb3653936e23dc
Nathan Hughes <20962973+Killerherts@users.noreply.github.com>
2023-10-25 11:42:33 -0500

    Update soilSensorFeeding.js
    Moved are vars that need to be changed 2 the top. Modified process flow so that payload are built dynamically so that users don't need to have a bunch of call service nodes

--- soilSensorFeeding.js
+++ soilSensorFeeding.js
@@ -1,30 +1,43 @@
-// Constants
+// Constants for Home Assistant Entity IDs
+const ENTITY_IDS = {
+    highestSoilSensor: 'input_number.highest_soil_sensor_value_side_1',
+    generative: 'input_boolean.side1_generative_steering',
+    flipToFlower: 'input_boolean.side1_filp_to_flower',
+    lightOnTime: 'input_datetime.side_1_lights_on_time',
+    soilMoisture: 'sensor.soil_sesor_a1_moisture_wc',
+    maintenancePhase: 'input_boolean.side1_maintance_phase',
+    feedPumpSwitch: 'switch.side_1_feed_pump_switch'
+};
+
+// Other Constants
 const SECONDS_IN_DAY = 24 * 60 * 60;
-const MIN_IRRIGATION_FREQUENCY = 6 * 60; // 10 minutes in seconds
-const DESIRED_MOISTURE = 45; // Desired moisture level in water content percentage
+const MIN_IRRIGATION_FREQUENCY = 1 * 60; // 10 minutes in seconds
+const DESIRED_MOISTURE = 46; // Desired moisture level in water content percentage
 const P1_THRESHOLD = 2;
-const P2_THRESHOLD = 5;
-const MAX_DELTA = 15;
+const P2_THRESHOLD = 5; //dryback % before sending a p2
+const MAX_DELTA = 18; //max dryback overnight
+const DELAY_FOR_P1_FEED = 25;  // in seconds
+const DELAY_FOR_P2_FEED = 35;  // in seconds
+const debug = true;
 
-// Set the debug flag
-const debug ='true';
 
-// Retrieve necessary data from Home Assistant
-let generative = getHAState('input_boolean.side_2_generative_steering') === 'on';
-let flipToFlower = getHAState('input_boolean.side2_flip_to_flower') === 'on';
-let lightOnTime = convertTime(getHAState('input_datetime.side_2_lights_on_time'));
-let soilMoisture = parseFloat(getHAState('sensor.soil_sesor_a1_moisture_wc'));
-let maintenancePhase = getHAState('input_boolean.side_2_maintance_phase') === 'on';
+// For retrieving data:
+let highestSoilsensorVal = 42.00; //getHAState(ENTITY_IDS.highestSoilSensor);
+let generative = getHAState(ENTITY_IDS.generative) === 'on';
+let flipToFlower = getHAState(ENTITY_IDS.flipToFlower) === 'on';
+let lightOnTime = convertTime(getHAState(ENTITY_IDS.lightOnTime));
+let soilMoisture = 42.00; //parseFloat(getHAState(ENTITY_IDS.soilMoisture));
+let maintenancePhase = getHAState(ENTITY_IDS.maintenancePhase) === 'on';
+debugWarn('maintenacePhase: ' + maintenancePhase)
 let currentTime = getCurrentTime();
 let currentTimeUTC = getCurrentTimeUTC();
 let timeSinceLastIrrigation;
 
 // Calculate parameters
-let dryBackThreshold = DESIRED_MOISTURE - 3;
 let lightOffTime = calculateLightOffTime(flipToFlower, lightOnTime);
 let irrigationStart = calculateIrrigationStart(generative, lightOnTime);
 let irrigationEnd = calculateIrrigationEnd(lightOffTime);
-let lastChangedTimeMs = new Date(global.get('homeassistant').homeAssistant.states['switch.side_2_feed_pump_switch'].last_changed).getTime();
+let lastChangedTimeMs = new Date(global.get('homeassistant').homeAssistant.states[ENTITY_IDS.feedPumpSwitch].last_changed).getTime();
 let lastChanged = convert_epoch_to_utc_seconds(lastChangedTimeMs);
 let inIrrigationWindow = checkInIrrigationWindow(currentTime, irrigationStart, irrigationEnd);
 
@@ -34,8 +47,6 @@ if (lastChanged < currentTimeUTC) {
     timeSinceLastIrrigation = Math.floor((SECONDS_IN_DAY - lastChanged) + currentTimeUTC);
 }
 
-
-//node.warn('time since last irrigation ' + timeSinceLastIrrigation);
 
 // Function to get current time in UTC seconds
 function getCurrentTimeUTC() {
@@ -52,7 +63,7 @@ function convert_epoch_to_utc_seconds(epoch_ms) {
 }
 
 
-// Function to convert time
+// Function to convert time2 string
 function convertTime(timeString) {
     let time;
     let isUtc = false;
@@ -85,6 +96,37 @@ function convertTime(timeString) {
 function getHAState(state) {
     return global.get('homeassistant').homeAssistant.states[state].state;
 }
+
+/**
+ * Constructs a payload for Home Assistant service calls.
+ * 
+ * @param {string} service - The service to be called (e.g., 'turn_on', 'turn_off').
+ * @param {string} domain - The domain of the entity (e.g., 'switch', 'light').
+ * @param {string} entity_id - The id of the entity to be acted upon.
+ * @param {number} delay - The delay in seconds before the action is performed.
+ * @param {object} data - Any additional data to be passed along with the service call.
+ * @returns {object} - The constructed payload.
+ */
+function buildPayload(service, domain, entity_id, delay = null, data = {}) {
+    let payload = {
+        service: service,
+        domain: domain,
+        entity_id: entity_id,
+        data: data
+    };
+
+    let message = {
+        payload: payload
+    };
+
+    if (delay !== null) {
+        message.delay = delay * 1000; // Convert seconds to milliseconds
+    }
+
+    return message;
+}
+// Whenever you want to build a payload in your processControlFlow function:
+// const payload = buildPayload('turn_on', 'switch', 'switch.your_p1_feed_id', DELAY_FOR_P1_FEED);
 
 
 
@@ -128,28 +170,6 @@ function checkInIrrigationWindow(currentTime, irrigationStart, irrigationEnd) {
     }
 }
 
-function logDebugData() {
-    if (debug) {
-        node.warn("inIrrigationWindow: " + inIrrigationWindow);
-        node.warn("Generative: " + generative);
-        node.warn("Flip to flower: " + flipToFlower);
-        node.warn("Soil moisture: " + soilMoisture);
-        node.warn("Current time: " + currentTime);
-        node.warn("Irrigation start time: " + irrigationStart);
-        node.warn("Irrigation end time: " + irrigationEnd);
-        node.warn("last Irrigation Run " + lastChanged);
-    }
-}
-
-// Modify all your node.warn calls to check the debug flag
-function debugWarn(message) {
-    if (debug) {
-        node.warn(message);
-    }
-}
-
-
-// Process control flow
 // Process control flow
 function logDebugData() {
     if (debug) {
@@ -161,6 +181,8 @@ function logDebugData() {
         node.warn("Irrigation start time: " + irrigationStart);
         node.warn("Irrigation end time: " + irrigationEnd);
         node.warn("last Irrigation Run " + lastChanged);
+        node.warn("Highest Sensor Value: " + highestSoilsensorVal);
+        node.warn("Last changed Switch:" + lastChangedTimeMs + " ms " + lastChanged)
     }
 }
 
@@ -171,52 +193,53 @@ function debugWarn(message) {
     }
 }
 
-// Process control flow
 function processControlFlow() {
+    let turnOnOutput = null;
+    let delayAndTurnOffOutput = null;
+    let flipBooleanOutput = null;
+    let setInputNumberOutput = null;
+
     if (timeSinceLastIrrigation < MIN_IRRIGATION_FREQUENCY) {
         debugWarn(`Last irrigation was less than ${MIN_IRRIGATION_FREQUENCY / 60} minutes ago. Not performing a check now.`);
-        return [null, null, null, null, null];
+        return [null, null, null, null];
     }
 
     const moistureDifference = DESIRED_MOISTURE - soilMoisture;
 
     if (Math.abs(moistureDifference) > MAX_DELTA) {
-        debugWarn(`Moisture difference is greater than ${MAX_DELTA}%. Outputting to a new 5th return.`);
-        return [null, null, null, null, { payload: JSON.stringify(soilMoisture) }];
-    }
-
-    if (inIrrigationWindow) {
+        debugWarn("Max Dryback Feeding");
+        turnOnOutput = buildPayload('turn_on', 'switch', ENTITY_IDS.feedPumpSwitch);
+        delayAndTurnOffOutput = buildPayload('turn_off', 'switch', ENTITY_IDS.feedPumpSwitch, DELAY_FOR_P2_FEED);
+    } else if (inIrrigationWindow) {
         if (!maintenancePhase) {
-            if (moistureDifference > P1_THRESHOLD) {
-                debugWarn('P1 feed');
-                return [{ payload: soilMoisture.toString() }, null, null, null, null];
-            } else if (Math.abs(moistureDifference) <= 2) {
-                // If the current reading is within 2 percent of the desired moisture
+            if (moistureDifference <= P1_THRESHOLD || highestSoilsensorVal >= DESIRED_MOISTURE) {
                 debugWarn('P2 Flip Switch');
-                return [null, null, null, { payload: "Start P2" }, null];
+                turnOnOutput = buildPayload('turn_on', 'switch', ENTITY_IDS.feedPumpSwitch);
+                delayAndTurnOffOutput = buildPayload('turn_off', 'switch', ENTITY_IDS.feedPumpSwitch, DELAY_FOR_P2_FEED);
+            } else if (moistureDifference > P1_THRESHOLD) {
+                debugWarn('P1 feed');
+                turnOnOutput = buildPayload('turn_on', 'switch', ENTITY_IDS.feedPumpSwitch);
+                delayAndTurnOffOutput = buildPayload('turn_off', 'switch', ENTITY_IDS.feedPumpSwitch, DELAY_FOR_P1_FEED);
             }
-        }
-        if (moistureDifference > P2_THRESHOLD && maintenancePhase) {
+        } else if (moistureDifference > P2_THRESHOLD) {
             debugWarn('P2 feed');
-            return [null, { payload: soilMoisture.toString() }, null, null, null];
+            turnOnOutput = buildPayload('turn_on', 'switch', ENTITY_IDS.feedPumpSwitch);
+            delayAndTurnOffOutput = buildPayload('turn_off', 'switch', ENTITY_IDS.feedPumpSwitch, DELAY_FOR_P2_FEED);
         }
     } else {
-        // Handles the over midnight irrigation window scenario
         if ((irrigationStart < irrigationEnd && (currentTime > irrigationEnd || currentTime < irrigationStart)) ||
             (irrigationStart > irrigationEnd && !(currentTime > irrigationStart && currentTime < irrigationEnd))) {
-            debugWarn(`Outside of irrigation window. Resetting Maintenance Switch for New day.`);
-            return [null, null, { payload: 'reset switch' }, null, null];
-        } else {
-            debugWarn(`Soil Moisture within desired range`);
-            return [null, null, null, null, null];
+            if (maintenancePhase == true) {
+                debugWarn("Resetting Maintenance Switch");
+                setInputNumberOutput = buildPayload('set_value', 'input_number', ENTITY_IDS.highestSoilSensor, null, {value:0});  // Reset highestSoilSensor value to 0
+                flipBooleanOutput = buildPayload('turn_off', 'input_boolean', ENTITY_IDS.maintenancePhase);
+            }
         }
     }
+
+     return [turnOnOutput, delayAndTurnOffOutput, flipBooleanOutput, setInputNumberOutput];
 }
 
-// Run the debug function if debug is enabled
-if (debug) {
-    logDebugData();
-}
-
+logDebugData()
 // Run the processControlFlow function
 return processControlFlow();
